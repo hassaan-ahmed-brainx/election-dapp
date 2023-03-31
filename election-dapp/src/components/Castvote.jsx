1f894f2aa95cffbc83ba4eeb62781064d2e6aa22
Hassaan <hassaan.ahmed@brainxtech.com>
2023-03-31 17:17:49 +0500

    features-branch

--- election-dapp/src/components/Castvote.jsx
+++ election-dapp/src/components/Castvote.jsx
@@ -1,72 +1,78 @@
-import { useState } from "react";
-import { Link } from 'react-router-dom';
-import ContractAbi from '../abi.json'
-import * as Constants from  '/home/brainx/Desktop/dapp/src/constants/constants.js'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import * as Constants from "/home/brainx/Desktop/dapp/src/constants/constants.js";
 
+const Castavote = ({ candidatesData }) => {
+  const [selectedCandidate, setSelectedCandidate] = useState("");
+  const [voteCount, setVoteCount] = useState(0);
+  const [candidates, setCandidates] = useState([]);
 
+  useEffect(() => {
+    setCandidates(candidatesData.candidates);
+  }, [candidatesData.candidates]);
 
-const Castavote =(candidatesData)=> {
-    let {candidates} = candidatesData;
+  const handleVote = async () => {
+    try {
+      const accounts = await Constants.web3.eth.getAccounts();
+      const address = selectedCandidate.split(",")[1];
+      console.log(address);
+      console.log(accounts[0]);
+      await Constants.myContract.methods
+        .castVote(address)
+        .send({ from: accounts[0] });
+      console.log(accounts[0]);
+      const candidateIndex = candidates.findIndex((c) => c.address === address);
+      const updatedCandidates = [...candidates];
+      updatedCandidates[candidateIndex].votecount++;
+      setSelectedCandidate("");
+      setVoteCount(updatedCandidates[candidateIndex].votecount);
+      candidatesData.setCandidates(updatedCandidates);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    let [selectedCandidate, setSelectedCandidate] = useState('');
-    let [voteCount, setVoteCount] = useState(0);
+  return (
+    <div className="Menu">
+      <h1>Cast your vote</h1>
+      <Link className="MenuLink" to="/">
+        {" "}
+        Main menu
+      </Link>
+      <h1 className="TextCenter">Election Results</h1>
+      <hr />
 
-    let myContract;
-	const contractAddress = Constants.contractAddress
-    const web3 = Constants.web3;
-	myContract = new web3.eth.Contract(ContractAbi, contractAddress);
-
-    const handleVote = async () => {
-        try {
-          const accounts = await web3.eth.getAccounts();
-          const address = selectedCandidate.split(',')[1];
-          await myContract.methods.castVote(address).send({ from: accounts[0] });
-          const candidateIndex = candidates.findIndex(c => c.address === address);
-          const updatedCandidates = [...candidates];
-          updatedCandidates[candidateIndex].votecount++;
-          setSelectedCandidate('');
-          setVoteCount(updatedCandidates[candidateIndex].votecount);
-          candidatesData.setCandidates(updatedCandidates);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      
-
-    return (
-        <div className="Menu">
-            <h1>Cast your vote</h1>
-            <Link className='menu-link' to='/'> Main menu</Link>   
-            <h1 className="text-center">Election Results</h1>
-            <hr />
-
-            <div>
-                <table className="table-header">
-                    <th>Name </th>
-                    <th className="address-candidate">Address</th>
-                </table>
-                <ul>
-                    {candidates?.map((candidate)=>(
-                        <li key={candidate.address}>
-                            <strong>{candidate.name } {candidate.address} {candidate.votecount}</strong> 
-                            <button onClick={() => setSelectedCandidate(`${candidate.name},${candidate.address}`)}>Select</button>
-                        </li>
-                    ))}
-                </ul>
-                {selectedCandidate && (
-                    <div>
-                        <h3>Selected candidate: {selectedCandidate.split(',')[0]}</h3>
-                        <h3>Vote count: {voteCount}</h3>
-                        <button onClick={handleVote}>Vote</button>
-                    </div>
-                )}
-            </div>
-        </div>
-   
-
-    );
-}
+      <div>
+        <table className="TableHeader">
+          <th>Name </th>
+          <th className="AddressCandidate">Address</th>
+        </table>
+        <ul>
+          {candidates?.map((candidate) => (
+            <li key={candidate.address}>
+              <strong>
+                {candidate.name} {candidate.address} {candidate.votecount}
+              </strong>
+              <button
+                onClick={() =>
+                  setSelectedCandidate(`${candidate.name},${candidate.address}`)
+                }
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+        {selectedCandidate && (
+          <div>
+            <h3>Selected candidate: {selectedCandidate.split(",")[0]}</h3>
+            <h3>Vote count: {voteCount}</h3>
+            <button onClick={handleVote}>Vote</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Castavote;
-
-
--- election-dapp/src/components/Menu.jsx
+++ election-dapp/src/components/Menu.jsx
@@ -1,22 +1,19 @@
-import '/home/brainx/Desktop/dapp/src/index.css';
-import { Link } from 'react-router-dom';
+import "/home/brainx/Desktop/dapp/src/index.css";
+import { Link } from "react-router-dom";
 
-
-const Menu =()=> {
-    return(
+const Menu = () => {
+  return (
+    <div>
+      <title>Election System</title>
+      <div className="Menu">
         <div>
-        <title>Election System</title>
-        <div className='Menu'>
-             <div >
-                <h1>Election DAPP</h1>
-                <Link to='/Addcandidate' > Add a Candidate</Link>
-                <Link to='/CastVote'> Cast a Vote</Link>
-            </div>
+          <h1>Election DAPP</h1>
+          <Link to="/Addcandidate"> Add a Candidate</Link>
+          <Link to="/CastVote"> Cast a Vote</Link>
         </div>
-        </div>
-
-    );
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
