65f7540488f0cf38755c5f19f95cb43d1d953738
Hassaan <hassaan.ahmed@brainxtech.com>
2023-03-31 15:56:12 +0500

    features-branch

--- election-dapp/src/App.js
+++ election-dapp/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from '../src/components/Menu';
+import Castavote from '../src/components/Castvote';
+import Addcandiate from '../src/components/Addcandidate';
+import { useState} from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
+  const [candidates, setcandidates] = useState([]);
+
+  const handleAddCandidate = (newCandidate) =>{
+    setcandidates([...candidates, newCandidate]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>       
+      <Router>
+        <Routes>
+          <Route path="/" element={<Menu/>} />
+          <Route path="/Castvote" element={<Castavote candidates={candidates}/>} />
+          <Route path="/Addcandidate" element={<Addcandiate handleAddCandidate={handleAddCandidate}/>}/>
+        </Routes>     
+      </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ election-dapp/src/components/Addcandidate.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import ContractAbi from '../abi.json';
+import * as Constants from  '/home/brainx/Desktop/dapp/src/constants/constants.js'
+
+
+const Addcandiate =(candidatesData)=> {
+
+    let [addressCandidate, setAddressCandidate] = useState(localStorage.getItem('candidate_address') || '');
+    let [addressCandidate1, setAddressCandidate1] = useState(localStorage.getItem('candidate_name') || '');
+
+    let { handleAddCandidate } = candidatesData;
+    let myContract;
+	const contractAddress = Constants.contractAddress
+    const web3 = Constants.web3;
+	myContract = new web3.eth.Contract(ContractAbi, contractAddress);
+
+
+
+    const handleChange1 = event => {
+        setAddressCandidate(event.target.value);
+    }
+
+    const handleChange2 = event => {
+        setAddressCandidate1(event.target.value);
+    }
+
+    let handleAddCandidate1 = async () => {
+        await myContract.methods.addCandidate(addressCandidate, addressCandidate1).send({ from: window.ethereum.selectedAddress });
+        let newcandidate = { address: addressCandidate, name: addressCandidate1, votecount:0 };
+        handleAddCandidate(newcandidate);
+        setAddressCandidate("");
+        setAddressCandidate1("");
+    }
+
+    return(
+        <div className='Menu'>
+            <Link className='menu-link' to='/'> Main menu</Link>   
+            <h1>Add a Candidate</h1> 
+            <div>
+                <label>Candidate Address: </label>
+                <input
+                    type="text"
+                    name=""
+                    id="c_address"
+                    placeholder="Adress of candidate"
+                    className='candidateaddress'
+                    required
+                    onChange={handleChange1}
+                    value={addressCandidate}
+                />
+            </div>  
+
+            <div className='candidate-div'>
+                <label className="candidatename" >Candidate Name: </label>
+                <input
+                    type="text"
+                    name=""
+                    id="c_name"
+                    placeholder="Name of candidate"
+                    className='candidate-input'
+                    required
+                    onChange={handleChange2}
+                    value={addressCandidate1}
+                />
+            </div>
+            <button onClick={handleAddCandidate1}>Submit</button>         
+        </div>
+    );
+}
+
+export default Addcandiate;
