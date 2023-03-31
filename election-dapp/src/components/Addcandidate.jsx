eee86acfa5068f285c22eb345442335dc9b9c09b
hassaan-ahmed-brainx <124145815+hassaan-ahmed-brainx@users.noreply.github.com>
2023-03-31 18:08:14 +0500

    Update Addcandidate.jsx

--- election-dapp/src/components/Addcandidate.jsx
+++ election-dapp/src/components/Addcandidate.jsx
@@ -5,6 +5,7 @@ import * as Constants from "/home/brainx/Desktop/dapp/src/constants/constants.js
 const Addcandiate = (props) => {
   const [addressCandidate, setAddressCandidate] = useState("");
   const [addressCandidate1, setAddressCandidate1] = useState("");
+  const [handleAddCandidate, setHandleAddCandidate] = useState(null);
 
   const handleChange1 = (event) => {
     setAddressCandidate(event.target.value);
@@ -15,30 +16,26 @@ const Addcandiate = (props) => {
   };
 
   useEffect(() => {
-    const { handleAddCandidate } = props.candidatesData;
-    let handleAddCandidate1 = async () => {
-      await Constants.myContract.methods
-        .addCandidate(addressCandidate, addressCandidate1)
-        .send({ from: window.ethereum.selectedAddress });
-      let newcandidate = {
-        address: addressCandidate,
-        name: addressCandidate1,
-        votecount: 0,
-      };
-      handleAddCandidate(newcandidate);
-      setAddressCandidate("");
-      setAddressCandidate1("");
+    setHandleAddCandidate(props.candidatesData);
+  }, [props.candidatesData]);
+
+  const handleAddCandidate1 = async () => {
+    await Constants.myContract.methods
+      .addCandidate(addressCandidate, addressCandidate1)
+      .send({ from: window.ethereum.selectedAddress });
+    let newcandidate = {
+      address: addressCandidate,
+      name: addressCandidate1,
+      votecount: 0,
     };
-  }, [
-    props.candidatesData,
-    addressCandidate,
-    addressCandidate1,
-    Constants.myContract,
-  ]);
+    handleAddCandidate(newcandidate);
+    setAddressCandidate("");
+    setAddressCandidate1("");
+  };
 
   return (
     <div className="Menu">
-      <Link className="MenuLink" to="/">
+      <Link className="Menu_Link" to="/">
         {" "}
         Main menu
       </Link>
@@ -50,7 +47,7 @@ const Addcandiate = (props) => {
           name=""
           id="c_address"
           placeholder="Adress of candidate"
-          className="CandidateAddress"
+          className="Candidate_Address"
           required
           onChange={handleChange1}
           value={addressCandidate}
@@ -58,19 +55,19 @@ const Addcandiate = (props) => {
       </div>
 
       <div className="candidate-div">
-        <label className="CandidateName">Candidate Name: </label>
+        <label className="Candidate_Name">Candidate Name: </label>
         <input
           type="text"
           name=""
           id="c_name"
           placeholder="Name of candidate"
-          className="CandidateInput"
+          className="Candidate_Input"
           required
           onChange={handleChange2}
           value={addressCandidate1}
         />
       </div>
-      <button>Submit</button>
+      <button onClick={handleAddCandidate1}>Submit</button>
     </div>
   );
 };
