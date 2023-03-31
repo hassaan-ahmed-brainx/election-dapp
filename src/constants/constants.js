4a637a410906d9ecb4563eaaf723b63436d2393e
Hassaan <hassaan.ahmed@brainxtech.com>
2023-03-31 15:20:58 +0500

    initial commit

--- src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-import Web3 from "web3";
-
-export const provider = window.ethereum;
-export const contractAddress = '0x943919392a07A8e68BDDb226DcFE694A67c2DDFa';
-export const web3 = new Web3(provider);
